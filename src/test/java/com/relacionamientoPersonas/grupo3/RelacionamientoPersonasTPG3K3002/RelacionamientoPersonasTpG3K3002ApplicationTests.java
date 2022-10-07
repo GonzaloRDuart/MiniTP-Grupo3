e4df67413e723b81dd7a0acabd3fad18bac9f151
unknown <leomorsella@frba.utn.edu.ar>
2022-10-07 19:44:01 -0300

    commit inicial

---
+++ src/test/java/com/relacionamientoPersonas/grupo3/RelacionamientoPersonasTPG3K3002/RelacionamientoPersonasTpG3K3002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relacionamientoPersonas.grupo3.RelacionamientoPersonasTPG3K3002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionamientoPersonasTpG3K3002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
