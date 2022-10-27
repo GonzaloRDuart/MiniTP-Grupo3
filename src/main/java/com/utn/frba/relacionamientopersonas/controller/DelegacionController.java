ca451bba9ffe9c98763fd805990a6716d5a5cf66
unknown <leomorsella@frba.utn.edu.ar>
2022-10-27 12:11:31 -0300

    avances en ruteo de pantallas

--- src/main/java/com/utn/frba/relacionamientopersonas/controller/DelegacionController.java
+++ src/main/java/com/utn/frba/relacionamientopersonas/controller/DelegacionController.java
@@ -1,13 +1,46 @@
 package com.utn.frba.relacionamientopersonas.controller;
 
+import com.github.jknack.handlebars.Template;
 import com.utn.frba.relacionamientopersonas.model.delegacion.Delegacion;
+import com.utn.frba.relacionamientopersonas.model.memoryRepos.RepositorioDelegaciones;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.github.jknack.handlebars.Handlebars;
 
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
-@RestController
+import java.util.Map;
+
+@Controller
 public class DelegacionController {
-/*
+
+  private final RepositorioDelegaciones repoDelegaciones;
+  private final Handlebars handlebars;
+
+  public DelegacionController() {
+      this.repoDelegaciones = new RepositorioDelegaciones();
+      this.handlebars = new Handlebars();
+
+  }
+
+    @GetMapping(value = "/delegaciones", produces = MediaType.TEXT_HTML) //Evaular si cambiarlo a TEXT_HTML_TYPE
+    public ResponseEntity<String> obtenerVistaDeTodas(@RequestParam("sesion") String idSesion) throws IOException {
+      Template template = handlebars.compile("/templates/index");
+      List<Delegacion> delegaciones = repoDelegaciones.getDelegaciones();
+
+      Map<String, Object> model = new HashMap<>();
+      model.put("listadelegaciones", delegaciones);
+
+      String html = template.apply(model);
+
+      return ResponseEntity.status(200).body(html);
+  }
+
+  /*
     @Autowired
     private IDelegacionService interfazDelegacion;
 
--- src/main/java/com/utn/frba/relacionamientopersonas/controller/PersonaController.java
+++ src/main/java/com/utn/frba/relacionamientopersonas/controller/PersonaController.java
@@ -1,14 +1,52 @@
 package com.utn.frba.relacionamientopersonas.controller;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.utn.frba.relacionamientopersonas.model.memoryRepos.RepositorioPersonas;
 import com.utn.frba.relacionamientopersonas.model.persona.Persona;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@RestController
+@Controller
 public class PersonaController {
+    private final RepositorioPersonas repoPersonas;
+    private final Handlebars handlebars;
+
+    public PersonaController() {
+        this.repoPersonas = new RepositorioPersonas();
+        this.handlebars = new Handlebars();
+    }
+
+    @GetMapping(value = "/personas", produces = MediaType.TEXT_HTML)
+    public ResponseEntity<String> obtenerVistaDeTodas(@RequestParam("sesion") String idSesion) throws IOException {
+
+        //validar accion en capa modelo según roles o usuario asociados al idSesion
+        Template template = handlebars.compile("/templates/index");
+        List<Persona> personas = repoPersonas.getPersonas();
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("listamascotas", personas);
+
+        String html = template.apply(model);
+
+        return ResponseEntity.status(200).body(html);
+    }
+
+
+
+
+
+
+
   /*  @Autowired
     private IPersonaService intefazPersona;
 
